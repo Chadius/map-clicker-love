3d216f2ea107d54f9d8732c2f94e81e364e8d8b4
Chad Serrant <chadius@alum.mit.edu>
2018-12-02 12:50:52 -0500

    master: Map draws using the Graphics Context.

--- mapDrawing.lua
+++ mapDrawing.lua
@@ -9,7 +9,7 @@ end
 MapDrawing={}
 function MapDrawing:new(graphicsContext)
   self.movementTileToImageIndex={}
-  self.tileSize = graphicsContext:getTileSize()
+  self.graphicsContext = graphicsContext
   self.mapTileSpriteSheet = nil
   self.mapTileImageByTerrain = {}
   return self
@@ -41,28 +41,11 @@ function MapDrawing:load(mapJson)
 
   return self
 end
-local function getTileCoordinate(column, row, tileSize)
-  if column == nil or row == nil then
-    return nil, nil
-  end
-
-  -- Get the Y coordinate
-  local y = tileSize * (row - 1)
-
-  -- Get the X coordinate
-  local x = tileSize * (column - 1)
-
-  -- Based on the row, add an offset.
-  if row % 2 == 1 then
-    x = x + tileSize / 2
-  end
-
-  return x, y
-end
 local function drawTile(self, column, row, colorString)
   local quadIndex = self.movementTileToImageIndex[colorString]
   local tileImage = self.mapTileImageByTerrain[quadIndex]
-  local x, y = getTileCoordinate(column, row, self.tileSize)
+  local x, y = self.graphicsContext:getTileCoordinate(column, row)
+  local tileSize = self.graphicsContext:getTileSize()
 
   if tileImage ~= nil then
     -- Draw the tile
@@ -71,16 +54,17 @@ local function drawTile(self, column, row, colorString)
   else
     -- If the tile doesn't exist, we'll use a neon purple color if we can't find a backup color.
     love.graphics.setColor(0.84, 0.48, 0.72, 1)
-    love.graphics.rectangle("fill", x, y, self.tileSize, self.tileSize)
+    love.graphics.rectangle("fill", x, y, tileSize, tileSize)
   end
 
   -- Draw the outline around the tile
   love.graphics.setColor(0, 0, 0, 0.2)
-  love.graphics.rectangle("line", x, y, self.tileSize, self.tileSize)
+  love.graphics.rectangle("line", x, y, tileSize, tileSize)
 end
 function MapDrawing:drawSelectedTile(column, row)
   -- Draw the selected square located at the given column and row.
-  local x, y = getTileCoordinate(column, row, self.tileSize)
+  local x, y = self.graphicsContext:getTileCoordinate(column, row)
+  local tileSize = self.graphicsContext:getTileSize()
   if x == nil or y == nil then return end
 
   -- Choose the color based on the time elapsed.
@@ -93,10 +77,10 @@ function MapDrawing:drawSelectedTile(column, row)
 
   -- Draw the outlined tile.
   love.graphics.setColor(intensity, intensity, intensity)
-  love.graphics.rectangle("fill", x - 1, y - 1, self.tileSize, 3, 0.7)
-  love.graphics.rectangle("fill", x - 1, y - 1 + 3, 3, self.tileSize, 0.7)
-  love.graphics.rectangle("fill", x + self.tileSize - 1, y - 1, 3, self.tileSize, 0.7)
-  love.graphics.rectangle("fill", x + 1, y + self.tileSize - 1, self.tileSize + 1, 3, 0.7)
+  love.graphics.rectangle("fill", x - 1, y - 1, tileSize, 3, 0.7)
+  love.graphics.rectangle("fill", x - 1, y - 1 + 3, 3, tileSize, 0.7)
+  love.graphics.rectangle("fill", x + tileSize - 1, y - 1, 3, tileSize, 0.7)
+  love.graphics.rectangle("fill", x + 1, y + tileSize - 1, tileSize + 1, 3, 0.7)
 end
 function MapDrawing:draw(mapTile)
   -- For each row
@@ -112,18 +96,19 @@ function MapDrawing:getTileClickedOn(x, y, width, height)
   --[[ Returns the column and row of the tile clicked.
       Returns nil, nil if there is no such tile.
   --]]
+  local tileSize = self.graphicsContext:getTileSize()
 
   -- Based on the y coordinate, determine the row.
-  row = math.floor(y / self.tileSize) + 1
+  row = math.floor(y / tileSize) + 1
 
   -- The row will determine the offset.
   offset = 0
   if row % 2 == 1 then
-    offset = self.tileSize / 2
+    offset = tileSize / 2
   end
 
   -- Remove the offset from the x coordinate. Based on the x coordinate, determine the column.
-  column = math.floor((x - offset) / self.tileSize) + 1
+  column = math.floor((x - offset) / tileSize) + 1
 
   -- make sure the row and column are valid.
   if row < 1 or column < 1 then
