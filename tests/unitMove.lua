7160996ef1df653cffa5a55c963b6ed2f8750cd2
Chad Serrant <chadius@alum.mit.edu>
2019-01-03 20:53:09 -0500

    unitMove: Writing skeletons for unit testing.

--- tests/unitMove.lua
+++ tests/unitMove.lua
@@ -1,3 +1,13 @@
+require "map/mapClass"
+require "tests/utility/map"
+local lunit = require "libraries/unitTesting/lunitx"
+
+if _VERSION >= 'Lua 5.2' then
+    _ENV = lunit.module('enhanced','seeall')
+else
+    module( "enhanced", package.seeall, lunit.testcase )
+end
+
 --[[
 RAW NOTES
 
@@ -69,3 +79,121 @@ GetTilesWithinMovement returns a table with elements, (1,3) (2,3) (1, 4)
 ChartCourse to (1,1), returns nil because you lack movement.
 NextWaypoint returns nil because you passed it nil.
 ]]
+
+local test_map = nil
+local test_unit = nil
+
+function setup()
+  -- Test Map is a 3x3 grid
+  testMap = MapClass:new{}
+  testMap.mapTile = {
+    {1,1,1,1,1},
+    {1,1,1,1,1},
+    {1,1,1,1,1},
+    {1,1,1,1,1},
+    {1,1,1,1,1},
+  }
+  testMap.moveTile = {
+    {1,2,1,1,1},
+    {3,1,1,1,1},
+    {1,1,1,1,1},
+    {1,1,4,1,5},
+    {1,1,4,1,1},
+  }
+
+  -- TODO Test Unit
+end
+
+function teardown()
+  testMap = nil
+  testUnit = nil
+end
+
+function assert_map_locations_table_found(expected_locations, actual_map, assert_prepend)
+  -- Asserts all of the expected and actual locations were found.
+  -- actual_map is a sparse nested table.
+  --- The first key is the column and the value is another table.
+  --- The inner table uses rows as keys.
+  -- expected_locations is a list of tables with column and row keys.
+  -- assert_prepend is a string to add to the assert if it fails.
+
+  for index, location in pairs(expected_locations) do
+    local column = location["column"]
+    local row = location["row"]
+
+    assert_not_equal(
+      nil,
+      actual_map[column],
+      assert_prepend .. ": cannot find column " .. column
+    )
+    assert_not_equal(
+      nil,
+      actual_map[column][row],
+      assert_prepend .. ": cannot find location " .. column .. ", " .. row
+    )
+  end
+
+  -- Everything matches.
+end
+
+function assert_map_locations_list_found(expected_locations, actual_map, assert_prepend)
+  -- Asserts all of the expected and actual locations were found.
+  -- Assumes actual and expected are a list of tables with column and row keys.
+
+  -- Should have visited the expected number of locations
+  assert_equal(#expected_locations, #actual_map)
+
+  -- For each expected location
+  for i, expected in ipairs(expected_locations) do
+    -- Look through the actual visited locations to see if they match.
+    for j, actual in ipairs(actual_map) do
+      if actual["column"] == expected["column"] and actual["row"] == expected["row"] then
+        -- Found this location. Mark it as true and look at the next expected one.
+        expected["found"] = true
+        break
+      end
+    end
+  end
+
+  -- Assert all of them were found.
+  for i, expected in ipairs(expected_locations) do
+    assert_true(assert_prepend .. ": " .. expected["found"], "(" .. expected["column"] .. "," .. expected["row"] ..") not found")
+  end
+end
+
+function test_unit_has_no_move()
+  -- Unit cannot move.
+  testUnit.movement=UnitMove:new(
+    testMap,
+    {column=3,row=2},
+    0,
+    "foot"
+  )
+
+  -- Unit can only access its spawn point.
+  local nearby_tiles = testUnit.movement.GetTilesWithinMovement()
+  local expected_locations = {
+    {column=3,row=2},
+  }
+  assert_map_locations_table_found(expected_locations, nearby_tiles, "test_unit_has_no_move")
+
+  local list_of_tiles = testUnit.movement.GetTilesWithinMovement{flatten=true}
+  assert_map_locations_list_found(expected_locations, list_of_tiles, "test_unit_has_no_move")
+
+  -- Unit can't chart courses because it can't move there.
+  -- Unit cannot get a waypoint because you cannot move.
+end
+
+function test_unit_with_1_move_fly()
+  -- Unit has 1 movement while flying
+  -- Unit can access adjacent spaces next turn (except the wall)
+  -- Unit can chart courses to anywhere except walls.
+  -- Chart a course that will take 2 turns to reach. NextWaypoint returns an adjacent tile.
+end
+
+function test_unit_with_1_move_foot()
+  -- Unit has 1 movement on foot
+  -- Unit can access adjacent spaces next turn.
+  -- Unit can chart courses to concrete terrain.
+  -- Chart course to a location that takes 2 movement to cross, expect nil.
+end
